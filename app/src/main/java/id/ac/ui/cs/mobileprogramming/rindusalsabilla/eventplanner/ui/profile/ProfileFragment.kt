33cfa3ac4f89c650affb2011564febb65f8c62e6
rindu <rindusalsabilla74@yahoo.com>
2020-12-31 20:58:26 +0700

    opengl

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rindusalsabilla/eventplanner/ui/profile/ProfileFragment.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rindusalsabilla/eventplanner/ui/profile/ProfileFragment.kt
@@ -4,6 +4,8 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
+import android.opengl.GLES10
+import android.opengl.GLSurfaceView
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -18,6 +20,7 @@ import id.ac.ui.cs.mobileprogramming.rindusalsabilla.eventplanner.databinding.Fr
 import id.ac.ui.cs.mobileprogramming.rindusalsabilla.eventplanner.ui.auth.LoginActivity
 import id.ac.ui.cs.mobileprogramming.rindusalsabilla.eventplanner.utils.EventPlannerConstant
 import kotlinx.android.synthetic.main.fragment_profile.view.*
+import javax.microedition.khronos.opengles.GL10
 
 class ProfileFragment : Fragment() {
     private lateinit var myView: View
@@ -44,6 +47,7 @@ class ProfileFragment : Fragment() {
             binding.viewImage.setImageBitmap(bitmap)
         })
 
+        binding.glSurface?.setRenderer(GLRenderer())
 
         myView.edit_profile.setOnClickListener {
             val intent: Intent = Intent(myView.context, ChangeProfileActivity::class.java)
@@ -66,6 +70,24 @@ class ProfileFragment : Fragment() {
         val intent = Intent(context, LoginActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
+    }
+
+    class GLRenderer : GLSurfaceView.Renderer {
+        var color = 0f
+        var colorVelocity = 1f/60f
+
+        override fun onDrawFrame(gl: GL10){
+            if (color > 1 || color < 0){
+                colorVelocity = -colorVelocity
+            }
+            color += colorVelocity
+
+            gl.glClearColor(color * 22f, color, color, 600f)
+            gl.glClear(GLES10.GL_COLOR_BUFFER_BIT)
+        }
+
+        override fun onSurfaceCreated(p0: GL10?, p1: javax.microedition.khronos.egl.EGLConfig?) {}
+        override fun onSurfaceChanged(gl: GL10, width: Int, height: Int){}
     }
 
 }
