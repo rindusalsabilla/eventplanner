95b12305b4787ee99c94d3d240247e6e4aa11d4b
rindu <rindusalsabilla74@yahoo.com>
2021-01-01 13:17:26 +0700

    permission

--- app/src/main/java/id/ac/ui/cs/mobileprogramming/rindusalsabilla/eventplanner/ui/dashboard/CountEventActivity.kt
+++ app/src/main/java/id/ac/ui/cs/mobileprogramming/rindusalsabilla/eventplanner/ui/dashboard/CountEventActivity.kt
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.mobileprogramming.rindusalsabilla.eventplanner.ui.dashboard
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil.setContentView
@@ -9,6 +10,18 @@ import kotlinx.android.synthetic.main.count_activity.*
 class CountEventActivity : AppCompatActivity()  {
     private var num: Int = 100
     private external fun minNumber(num: Int): Int
+
+    private val CAMERA_REQUEST_CODE = 100
+    private val STORAGE_REQUEST_CODE = 101
+
+    private val IMAGE_PICK_CAMERA_CODE = 102
+    private val IMAGE_PICK_GALLERY_CODE = 103
+
+    private lateinit var cameraPermission: Array<String>
+    private lateinit var storagePermission: Array<String>
+
+    private var imagerUri: Uri? = null
+
 
     companion object {
         init {
@@ -25,5 +38,20 @@ class CountEventActivity : AppCompatActivity()  {
             txtNum.text = num.toString()
         }
 
+        cameraPermission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.CAMERA)
+        storagePermission = arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+        imageView.setOnClickListener{
+            imagePickDialog()
+        }
+
+
+
+
+    }
+
+    private fun imagePickDialog(){
+        val options = arrayOf("Camera", "Gallery")
+        
     }
 }
